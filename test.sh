1645079881673fb8abab9b02668e41926ef22d61
Daniel Barter <danielbarter@gmail.com>
2021-06-28 12:29:31 -0700

    built a test

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
+Red="\033[0;31m"          # Red
+Green="\033[0;32m"        # Green
+Color_Off="\033[0m"       # Text Reset
+
 cp ./test_materials/rn.sqlite ./test_materials/rn_copy.sqlite
 
 ./build/RNMC --database=./test_materials/rn_copy.sqlite --number_of_simulations=1000 --base_seed=1000 --thread_count=8 --step_cutoff=200
@@ -8,9 +12,10 @@ sqlite3 ./test_materials/rn_with_trajectories.sqlite "${sql}" > ./test_materials
 sqlite3 ./test_materials/rn_copy.sqlite "${sql}" > ./test_materials/copy_trajectories
 
 if  cmp ./test_materials/trajectories ./test_materials/copy_trajectories; then
-    echo "trajectories same"
+    echo -e "${Green} passed: no difference in trajectories ${Color_Off}"
 else
-    echo "trajectories different"
+    echo -e "${Red} failed: difference in trajectories ${Color_Off}"
+fi
 
 rm ./test_materials/rn_copy.sqlite
 rm ./test_materials/trajectories
